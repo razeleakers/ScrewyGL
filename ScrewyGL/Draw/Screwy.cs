c3a55e2787410f16eb931a2dec99eb1448f9ad40
Happy Life <134342405+hluciana@users.noreply.github.com>
2024-05-17 01:35:13 -0500

    Add files via upload

---
+++ ScrewyGL/Draw/Screwy.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Text;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace ScrewyGL.Draw
+{
+    public class ScrewyPoint2D
+    {
+        public Guid ID { get; }
+        public string Type { get; set; }
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+        public Color ColorARGB { get; set; }
+
+        public ScrewyPoint2D(string Type,int PosX, int PosY, Color ColorARGB)
+        {
+            ID = Guid.NewGuid();
+            this.Type = Type;
+            this.PosX = PosX;
+            this.PosY = PosY;
+            this.ColorARGB = ColorARGB;
+        }
+    }
+
+    public class ScrewyRegister2D
+    {
+        private readonly StringBuilder sb_actualContent;
+        private readonly StringBuilder sb_lastContent;
+        private readonly List<ScrewyPoint2D> pointsData;
+
+        public ScrewyRegister2D() 
+        {
+            sb_actualContent = new StringBuilder();
+            sb_lastContent = new StringBuilder();
+            pointsData = new List<ScrewyPoint2D>();
+        }
+
+        public void AddNewPoint(ScrewyPoint2D point)
+        {
+            pointsData.Add(point);
+        }
+
+        public void CreateNewRegister()
+        {
+            if (pointsData.Count > 0)
+            {
+                sb_lastContent.Clear();
+
+                Color lastColor = pointsData[0].ColorARGB;
+
+                sb_actualContent.AppendLine("glBegin(" + pointsData[0].Type + ");");
+                sb_actualContent.AppendLine("glColor3ub(" + pointsData[0].ColorARGB.R + "," + pointsData[0].ColorARGB.G + "," + pointsData[0].ColorARGB.B + ");");
+
+                sb_lastContent.AppendLine("glBegin(" + pointsData[0].Type + ");");
+                sb_lastContent.AppendLine("glColor3ub(" + pointsData[0].ColorARGB.R + "," + pointsData[0].ColorARGB.G + "," + pointsData[0].ColorARGB.B + ");");
+
+                pointsData.ForEach((p) =>
+                {
+                    if (lastColor != p.ColorARGB)
+                    {
+                        sb_actualContent.AppendLine("glColor3ub(" + pointsData[0].ColorARGB.R + "," + pointsData[0].ColorARGB.G + "," + pointsData[0].ColorARGB.B + ");");
+                        sb_lastContent.AppendLine("glColor3ub(" + pointsData[0].ColorARGB.R + "," + pointsData[0].ColorARGB.G + "," + pointsData[0].ColorARGB.B + ");");
+                    }
+
+                    sb_actualContent.AppendLine("glVertex2d(" + p.PosX + "," + p.PosY + ");");
+                    sb_lastContent.AppendLine("glVertex2d(" + p.PosX + "," + p.PosY + ");");
+                });
+
+                sb_actualContent.AppendLine("glEnd();\n");
+                sb_lastContent.AppendLine("glEnd();\n");
+
+                pointsData.Clear();
+            }
+        }
+
+        public void ClearRegister()
+        {
+            sb_actualContent.Clear();
+            sb_lastContent.Clear();
+            pointsData.Clear();
+        }
+        
+        public string GetAllDraw(int panelWidth,int panelHeight)
+        {
+            if (sb_actualContent.Length == 0) return "Data empty...";
+
+            return "#include <GL/glut.h>\n\n" +
+                    "void ScrewyGL() {\n" +
+                    sb_actualContent.ToString().TrimEnd() +
+                    "\n}\n\n" +
+
+                    "void loadConfig() {\n" +
+                    "glLoadIdentity();\n" +
+                    "gluOrtho2D(0, "+ panelWidth + ", 0, " + panelHeight + ");\n" +
+                    "glClearColor(0.784f, 0.784f, 0.784f, 1.0f);\n" +
+                    "glClear(GL_COLOR_BUFFER_BIT);\n" +
+                    "glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);\n" +
+                    "glEnable(GL_BLEND);\n" +
+                    "ScrewyGL();\n" +
+                    "glFlush();\n" +
+                     "}\n\n" +
+
+                    "int main(int argc, char* argv[]) {\n" +
+                    "glutInit(&argc, argv);\n" +
+                    "glutInitDisplayMode(GLUT_SINGLE);\n" +
+                    "glutInitWindowSize(" + panelWidth + ", " + panelHeight + ");\n" +
+                    "glutInitWindowPosition(50, 50);\n" +
+                    "glutCreateWindow(\"ScrewyGL - BY M3RFR3T | github.com/RazeLeakers\");\n" +
+                    "glutDisplayFunc(loadConfig);\n" +
+                    "glutMainLoop();\n" +
+                    "return 0;\n" +
+                    "}";
+        }
+
+        public string GetActualDraw()
+        {
+            if (sb_actualContent.Length == 0) return "Data empty...";
+
+            return sb_actualContent.ToString().TrimEnd();
+        }
+
+        public string GetLastDraw()
+        {
+            if (sb_lastContent.Length == 0) return "Data empty...";
+
+            return sb_lastContent.ToString().TrimEnd();
+        }
+    }
+}
---
+++ ScrewyGL/Forms/SGL.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using ScrewyGL.Draw;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ScrewyGL
+{
+    public partial class SGL : Form
+    {
+        private int cantPoints = 0;
+        private bool infinitePoints = false;
+        private readonly ScrewyRegister2D screwyRegister2D;
+
+        public SGL()
+        {
+            InitializeComponent();
+            CB_Geometry.Text = "GL_LINES";
+            BTN_Stop.Enabled = false;
+            BTN_Result.Enabled = false;
+            screwyRegister2D = new ScrewyRegister2D();
+        }
+
+        private void NewButton(int x,int y)
+        {
+            Button btn = new Button
+            {
+                Cursor = Cursors.Hand,
+                FlatStyle = FlatStyle.Flat,
+                BackColor = BTN_Color.BackColor,
+                Size = new Size(7, 7),
+                Location = new Point(x - 3, y)
+            };
+
+            btn.MouseClick += (sender,e) => {
+                if (BTN_NewPoint.Enabled) return;
+
+                if(MessageBox.Show("Color: RGB(" + btn.BackColor.R + "," + btn.BackColor.G + "," + btn.BackColor.B + ");\n" +
+                    "Position: (X: " + (btn.Location.X + 3) + ",Y: " + (PB_Image.Height - btn.Location.Y) + ");",
+                    "Do you want to add this point to your draw?", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                {
+                    btn.BackColor = BTN_Color.BackColor;
+
+                    screwyRegister2D.AddNewPoint(new ScrewyPoint2D(CB_Geometry.SelectedItem.ToString(), (btn.Location.X + 3), (PB_Image.Height - btn.Location.Y), BTN_Color.BackColor));
+
+                    if (!infinitePoints) cantPoints--;
+                    if (cantPoints == 0 && !infinitePoints)
+                    {
+                        CB_Geometry.Enabled = true;
+                        BTN_NewPoint.Enabled = true;
+                        BTN_Result.Enabled = true;
+                        screwyRegister2D.CreateNewRegister();
+                    }
+                }
+            };
+
+            PB_Image.Controls.Add(btn);
+        }
+
+        private void PB_Image_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (cantPoints == 0 && !infinitePoints) return;
+
+            NewButton(e.X,e.Y);
+            screwyRegister2D.AddNewPoint(new ScrewyPoint2D(CB_Geometry.SelectedItem.ToString(),e.X, (PB_Image.Height - e.Y),BTN_Color.BackColor));
+
+            if (!infinitePoints) cantPoints--;
+            if (cantPoints == 0 && !infinitePoints)
+            {
+                CB_Geometry.Enabled = true;
+                BTN_NewPoint.Enabled = true;
+                BTN_Result.Enabled = true;
+                screwyRegister2D.CreateNewRegister();
+            }
+        }
+
+        private void PB_Image_MouseMove(object sender, MouseEventArgs e)
+        {
+            LB_Coordinates.Text = "Coordinates:\r\nX: " + e.X + "\r\nY: " + (PB_Image.Height - e.Y);
+        }
+
+        private void BTN_Upload_Picture_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog fileDG = new OpenFileDialog())
+            {
+                fileDG.Title = "Select a image";
+                fileDG.Filter = "Images (*.jpg, *.jpeg, *.png, *.gif, *.bmp)|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+                fileDG.Multiselect = false;
+
+                if (fileDG.ShowDialog() == DialogResult.OK)
+                {
+                    PB_Image.ImageLocation = fileDG.FileName;
+                }
+            }
+        }
+
+        private void BTN_Remove_Picture_Click(object sender, EventArgs e)
+        {
+            PB_Image.ImageLocation = "";
+        }
+
+        private void BTN_Color_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog colorDG = new ColorDialog())
+            {
+                if (colorDG.ShowDialog() == DialogResult.OK)
+                {
+                    BTN_Color.BackColor = colorDG.Color;
+                }
+            }
+        }
+
+        private void BTN_NewPoint_Click(object sender, EventArgs e)
+        {
+            CB_Geometry.Enabled = false;
+            BTN_NewPoint.Enabled = false;
+            BTN_Result.Enabled = false;
+
+            switch (CB_Geometry.SelectedItem.ToString())
+            {
+                case "GL_LINES":
+                    cantPoints = 2;
+                    break;
+                case "GL_TRIANGLES":
+                    cantPoints = 3;
+                    break;
+                case "GL_QUADS":
+                    cantPoints = 4;
+                    break;
+                case "GL_LINE_LOOP":
+                case "GL_POLYGON":
+                    BTN_Stop.Enabled = true;
+                    infinitePoints = true;
+                    break;
+            }
+        }
+
+        private void BTN_Stop_Click(object sender, EventArgs e)
+        {
+            infinitePoints = false;
+            CB_Geometry.Enabled = true;
+            BTN_NewPoint.Enabled = true;
+            BTN_Stop.Enabled = false;
+            BTN_Result.Enabled = true;
+            screwyRegister2D.CreateNewRegister();
+        }
+
+        private void BTN_ClearPoints_Click(object sender, EventArgs e)
+        {
+            cantPoints = 0;
+            infinitePoints = false;
+            CB_Geometry.Enabled = true;
+            BTN_NewPoint.Enabled = true;
+            BTN_Stop.Enabled = false;
+            BTN_Result.Enabled = false;
+            PB_Image.Controls.Clear();
+            screwyRegister2D.ClearRegister();
+        }
+
+        private void BTN_Result_Click(object sender, EventArgs e)
+        {
+            using (Result rt = new Result(screwyRegister2D.GetAllDraw(PB_Image.Width,PB_Image.Height), screwyRegister2D.GetActualDraw(), screwyRegister2D.GetLastDraw()))
+            {
+                rt.ShowDialog();
+            }
+        }
+    }
+}
