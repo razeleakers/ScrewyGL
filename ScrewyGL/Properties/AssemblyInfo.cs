983c3cd822cbdce9df00d869c78079a7d137fa80
Happy Life <134342405+hluciana@users.noreply.github.com>
2024-06-29 19:46:18 -0500

    Update AssemblyInfo.cs

--- ScrewyGL/Properties/AssemblyInfo.cs
+++ ScrewyGL/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ScrewyGL")]
-[assembly: AssemblyDescription("BY M3RFR3T | ScrewyGL v2.0")]
+[assembly: AssemblyDescription("M3RFR3T | ScrewyGL v2.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("github.com/RazeLeakers")]
 [assembly: AssemblyProduct("ScrewyGL")]
